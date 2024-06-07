17a2fe07b7197ff3327bd5fcc22463b8aa3fc483
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-06-07 15:03:40 +0200

    Create a new functions (summ and mult)

--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -1,3 +1,11 @@
 export function splitIntoWords(sentense: string) {
     return ['hello', 'my', 'friends']
 }
+
+export function summ(a: number, b: number) {
+    return a + b
+}
+
+export function mult(a: number, b: number) {
+    return a * b
+}
